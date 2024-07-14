1af0a9c59f38da116540c2c0501444c15cb22201
Dillan <dillankerai3@outlook.com>
2024-07-14 16:40:27 +0100

    fixed the sidebar logo image for smaller screen

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -14,19 +14,11 @@ export default function Logo({ width, height, style, className = '' }: LogoProps
       style={styles}
       className={`rsuite-logo logo-animated logo-animated-delay-half-seconds bounce-in ${className} `}
     >
-      <svg
-        viewBox="0 0 120 138"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        width="100%"
-        height="100%"
-        preserveAspectRatio="xMidYMin slice"
-      >
-        {/* ---------------------------------------- ADD GRADFLOW HERE. MAKE SURE ITS IN SVG FORMAT --------------------------------- */}
-        <image href="https://upload.wikimedia.org/wikipedia/commons/6/6d/Solid_pink.svg" x="0" y="0" width="120" height="138" />
-        {/* ---------------------------------------- --------------------------------------------------- */}
-
-      </svg>
+      <img
+        src="logo_no_text.png"
+        alt="Logo"
+        style={{ width: '100%', height: '100%' }}
+      />
     </div>
   );
 }
