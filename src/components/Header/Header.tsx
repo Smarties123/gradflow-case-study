2816eddc0eef92f35280a93cfe1e06470c597259
Dillan <dillankerai3@outlook.com>
2024-07-14 16:27:11 +0100

    managed to sort out the header for smaller screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -55,21 +55,7 @@ const renderAdminSpeaker = ({ onClose, left, top, className }: any, ref) => {
 };
 
 const renderNoticeSpeaker = ({ onClose, left, top, className }: any, ref) => {
-  const notifications = [
-    [
-      '7 hours ago',
-      'The charts of the dashboard have been fully upgraded and are more visually pleasing.'
-    ],
-    [
-      '13 hours ago',
-      'The function of virtualizing large lists has been added, and the style of the list can be customized as required.'
-    ],
-    ['2 days ago', 'Upgraded React 18 and Webpack 5.'],
-    [
-      '3 days ago',
-      'Upgraded React Suite 5 to support TypeScript, which is more concise and efficient.'
-    ]
-  ];
+
 
   return (
     <Popover ref={ref} className={className} style={{ left, top, width: 300 }} title="Last updates">
@@ -118,15 +104,15 @@ const Header = (props: HeaderProps) => {
 
 
       <Stack direction="row" spacing={4} alignItems="center">
-        <div style={{ display: 'flex', justifyContent: "space-between", width: "100%", alignItems: "center" }}>
+        <div style={{ display: 'flex', justifyContent: 'end', width: "100%", alignItems: "center" }}>
           <ButtonToolbar style={{ display: 'flex', gap: '3px' }}>
             <Button className="header-button" style={{ backgroundColor: '#8338ec', color: 'white', display: 'flex', alignItems: 'center', width: '120px' }}>
-              <FaPlus className="header-icon" style={{ fontSize: 18, color: 'white', margin: '1px 7px 1px 1px' }} />
+              <FaPlus className="header-icon" style={{ fontSize: 18, color: 'white', margin: '1px 1px 1px 1px' }} />
               <span className="visually-hidden">Add New</span> {/* Visually hidden text */}
 
             </Button>
             <Button className="header-button" style={{ backgroundColor: '#ff6200', color: 'white', display: 'flex', alignItems: 'center', width: '120px' }}>
-              <FaRegShareSquare className="header-icon" style={{ fontSize: 18, margin: '1px 7px 1px 1px' }} />
+              <FaRegShareSquare className="header-icon" style={{ fontSize: 18, margin: '1px 1px 1px 1px' }} />
               <span className="visually-hidden">Share</span> {/* Visually hidden text */}
 
             </Button>
