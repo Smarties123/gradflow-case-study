9d272ba7b8fce20186e4c6b654a4c5ad6ef557b6
Hemant <hemmant.smart@gmail.com>
2024-09-23 14:26:09 +0100

    Removed MUI

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Panel, PanelProps } from 'rsuite';
-import Copyright from '@/components/Copyright';
+import TermsAndConditions from '@/components/LandingPage/TermsAndConditions';  // Make sure this path is correct
 
 interface PageContentProps extends PanelProps {
-  showCopyright?: boolean;
+  showTermsAndConditions?: boolean;  // Rename prop to make it clear we are using Terms and Conditions
 }
 
 const PageContent = (props: PageContentProps) => {
-  const { showCopyright = true, ...panelProps } = props;
+  const { showTermsAndConditions = true, ...panelProps } = props;
+  
   return (
     <>
+      {/* Render the main panel content */}
       <Panel className="page-content" {...panelProps} />
-      {showCopyright && <Copyright />}
+      
+      {/* Conditionally render the Terms and Conditions */}
+      {showTermsAndConditions && <TermsAndConditions />}
     </>
   );
 };
