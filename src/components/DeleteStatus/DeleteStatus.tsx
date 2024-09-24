2a1e1edd6f82e92399cf02a6c4baf44a80391f62
dillan1 <dillan@teknobu.co.uk>
2024-09-24 18:31:28 +0100

    Delete Card Dark Mode and Board Scroll

--- src/components/DeleteStatus/DeleteStatus.tsx
+++ src/components/DeleteStatus/DeleteStatus.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import '../Modal/Modal.less'; // Ensure your Modal styles are linked
 
 const DeleteModal = ({ isOpen, onClose, onYes, onNo }) => {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    const handleDelete = () => {
-        onYes();  // Call the function passed in for delete confirmation
-        onClose(); // Close the modal after confirming deletion
-    };
+  const handleDelete = () => {
+    onYes(); // Call the function passed in for delete confirmation
+    onClose(); // Close the modal after confirming deletion
+  };
 
-    return (
-        <div className="modal-overlay" onClick={onClose}>
-            <div className={`modal-content`} onClick={(e) => e.stopPropagation()}>
-                <h2>Are You Sure?</h2>
-                <div className="modal-buttons">
-                    <button type="button" className="cancel-button" onClick={onNo}>
-                        Cancel
-                    </button>
-                    <button type="button" className="delete-button" onClick={handleDelete}>
-                        Delete
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className={`modal-content`} onClick={e => e.stopPropagation()}>
+        <h2>Are You Sure?</h2>
+        <div className="modal-buttons">
+          <button type="button" className="cancel-button" onClick={onNo}>
+            Cancel
+          </button>
+          <button type="button" className="delete-button" onClick={handleDelete}>
+            Delete
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DeleteModal;
