59ce35d49aad36cd7eed7341746d80cc01d32447
Dillan <dillankerai3@outlook.com>
2024-09-24 20:45:51 +0100

    Light Mode fix for Move Status

--- src/components/MoveStatusModal/MoveStatusModal.tsx
+++ src/components/MoveStatusModal/MoveStatusModal.tsx
@@ -59,7 +59,7 @@ const MoveStatusModal: React.FC<MoveStatusModalProps> = ({
     <div className="modal-overlay">
       <div className="modal-content" ref={modalRef} onClick={e => e.stopPropagation()}>
         <h2>Move Column</h2>
-        <div className="input-wrapper">
+        <div className="input-wrapper" id='input-wrapper'>
           <label className="bordered-label">Swap Column</label>
           <select
             className="border-input dropdown-input"
