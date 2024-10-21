b32c5309cf23bfc2192e78c23efb264236cd30fe
GH0ST555 <110325018+GH0ST555@users.noreply.github.com>
2024-10-21 21:30:02 +0100

    Made another change

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,7 +12,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,  // Hardcoded password for testing
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
   ssl: { rejectUnauthorized: false },
 });
 
