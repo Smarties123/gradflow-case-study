6119e99da666b0cbd0941110a3f6de2ec6db4040
Arjun Krishnan <110325018+GH0ST555@users.noreply.github.com>
2024-10-21 19:49:45 +0100

    Update statusController.js

--- backend/controllers/statusController.js
+++ backend/controllers/statusController.js
@@ -15,7 +15,7 @@ export const getStatuses = async (req, res) => {
     res.status(200).json(result.rows);
   } catch (error) {
     console.error('Error Fetching Statuses :', error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server Error' });
   }
 };
 
