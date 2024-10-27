613cd428cbec5890d7267184ec724f207f40b5d7
Dillan <dillankerai3@outlook.com>
2024-10-27 16:16:42 +0000

    Changing styling

--- src/components/Feedback/FeedbackPopup.tsx
+++ src/components/Feedback/FeedbackPopup.tsx
@@ -10,47 +10,45 @@ const FeedbackPopup: React.FC<{ show: boolean; onClose: () => void }> = ({ show,
       open={show}
       onClose={onClose}
       size="md"
-      backdrop="true" // Enables closing on outside click
+      backdrop={true} // Enables closing on outside click
       className="feedback-popup"
     >
-      <Modal.Header>
-        <Modal.Title>Feedback </Modal.Title>
-      </Modal.Header>
       <Modal.Body>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSf9mml8GbC5HsctSGR4f9UF54Hy89K4Oy5LVzRuW5m3F2fnMw/viewform?embedded=true"
           width="100%"
           height="500"
           frameBorder="0"
-        // marginHeight={0}
-        // marginWidth={0}
-        // title="Feedback Form"
         >
           Loading…
         </iframe>
       </Modal.Body>
-      <Modal.Footer>
-        <Button
-          appearance="ghost"
-          color="blue"
-          onClick={() => window.open('https://discord.gg/your-discord-link')}
-        >
-          <FaDiscord style={{ marginRight: '8px', verticalAlign: 'middle' }} />
-          Join Discord Community
-        </Button>
-        <Button
-          appearance="ghost"
-          color="green"
-          onClick={() => window.open('https://chat.whatsapp.com/your-whatsapp-link')}
-        >
-          <FaWhatsapp style={{ marginRight: '8px', verticalAlign: 'middle' }} />
-          Join WhatsApp Community
+      <Modal.Footer style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', flexGrow: 1, flexWrap: 'wrap', marginLeft: '35px' }}>
+          <Button
+            appearance="ghost"
+            color="blue"
+            onClick={() => window.open('https://discord.gg/hfVqgRDHc8')}
+            style={{ marginRight: '8px' }}
+          >
+            <FaDiscord style={{ marginRight: '4px', verticalAlign: 'text-top' }} />
+            Join Discord Community
+          </Button>
+          <Button
+            appearance="ghost"
+            color="green"
+            onClick={() => window.open('https://chat.whatsapp.com/JU6wzPwffwyIWKhwdnl79Z')}
+            style={{ marginLeft: '8px' }}
+          >
+            <FaWhatsapp style={{ marginRight: '4px', verticalAlign: 'text-top' }} />
+            Join WhatsApp Community
+          </Button>
+        </div>
+        {/* Close button at the bottom right */}
+        <Button onClick={onClose} appearance="subtle">
+          Close
         </Button>
       </Modal.Footer>
-      {/* Close button at the bottom right */}
-      {/* <Button className="close-button" onClick={onClose}>
-        Close
-      </Button> */}
     </Modal>
   );
 };
