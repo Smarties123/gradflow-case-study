290ba09445f9e173dc9c79e57ed557c07ab18b30
Dillan <dillankerai3@outlook.com>
2025-08-03 17:57:40 +0100

    Changing the Sign In Coming Soon Page

--- src/components/SignIn/ComingSoonSignIn.tsx
+++ src/components/SignIn/ComingSoonSignIn.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Button} from 'rsuite';
 import * as errors from '@/images/errors';
 import './ComingSoonSignIn.less';
 
-const ComingSoonSignIn = () => (
-  <div className="coming-soon-page">
-    <div className="item">
-      <img src={errors['Error404Img']} alt="Coming Soon" />
-      <div className="text">
-        <h3 className="coming-soon-message">
-          The Sign In with university feature is coming soon! We're hard at work creating exciting new features. Stay tuned for updates!
-        </h3>
-      </div>
-    </div>
+const ComingSoonSignIn = ({ onClose }) => (
+  <div className="coming-soon-container">
+    <img src={errors['Error404Img']} alt="Coming Soon" />
+    <h3 className="coming-soon-message-heading">
+      Coming Soon!
+    </h3>
+    <p className="coming-soon-message-text">
+      The Sign In with University feature is coming soon! We're hard at work creating exciting new features. Stay tuned for updates!
+    </p>
+    <Button
+      appearance="primary"
+      color="cyan"
+      onClick={onClose}
+      className="coming-soon-close-button"
+    >
+      Close
+    </Button>
   </div>
 );
-
 export default ComingSoonSignIn;
