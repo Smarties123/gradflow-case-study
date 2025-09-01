395e5d07dc3fbf72ac008b1dc3714e314f0310c0
Dillan <dillankerai3@outlook.com>
2025-09-01 21:00:50 +0100

    adding under construction site

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import PrivacyPolicyGDPR from './components/LandingPage/PrivacyPolicyGDPR';
 import AboutUs from './components/LandingPage/AboutUs';
 import Timeline from './components/LandingPage/TimelinePage';
 import ComingSoon from './components/ComingSoon/ComingSoon';
+import UnderConstruction from './components/UnderConstruction/UnderConstruction';
 import ComingSoonCalendar from './pages/calendar/ComingSoonCalendar';
 import Files from './pages/files/Files';
 import StripeCheckout from './components/StripeCheckout';
@@ -77,7 +78,7 @@ const App = () => {
 
       <Routes>
         {/* Public routes */}
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<UnderConstruction />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />} />
@@ -86,6 +87,7 @@ const App = () => {
         <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
         <Route path="/privacy-policy-GDPR" element={<PrivacyPolicyGDPR />} />
         <Route path="/comingsoon" element={<ComingSoon />} />
+        <Route path="/under-construction" element={<UnderConstruction />} />
         <Route path="/resend-verification" element={<ResendVerification />} />
         <Route path="/verify" element={<VerifyUser />} />
 
