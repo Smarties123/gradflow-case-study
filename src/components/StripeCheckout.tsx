83624b2a5a521d7ef079cf2dc12383be79e9ab62
Hemant <35453037+Smarties123@users.noreply.github.com>
2025-10-05 17:23:23 +0100

    Update StripeCheckout.tsx

--- src/components/StripeCheckout.tsx
+++ src/components/StripeCheckout.tsx
@@ -4,8 +4,7 @@ import { loadStripe } from '@stripe/stripe-js';
 
 const publishableKey =
   process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY ||
-  process.env.STRIPE_PUBLISHABLE_KEY ||
-  '';
+  process.env.STRIPE_PUBLISHABLE_KEY;
 
 const stripePromise = loadStripe(publishableKey);
 
